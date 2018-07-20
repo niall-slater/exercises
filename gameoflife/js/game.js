9949d02c988a9fc76711a7861e6ae408b6777a82
niall-slater <niallslater@gmail.com>
2018-07-20 18:52:59 +0100

    Reworked rendering to use canvas

--- gameoflife/js/game.js
+++ gameoflife/js/game.js
@@ -1,16 +1,77 @@
-let game = document.getElementById('game');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
-const size = 25;
+const tableSize = 64;
+const cellSize = 4;
 
-let rows = [];
+let tickDelay = 300;
 
-for (let x = 0; x < size; x++) {
-	let column = [];
-	for (let y = 0; y < size; y++) {
-		let cell = {};
-		column.push(cell);
-		game.innerHTML += 'o ';
+let charAlive = 'o';
+let charDead = '.';
+
+let table = [];
+
+const colors = {
+	alive: '#f2b630',
+	dead: '#333'
+};
+
+function init() {
+	
+	//build table
+	
+	for (let y = 0; y < tableSize; y++) {
+		let row = [];
+		for (let x = 0; x < tableSize; x++) {
+			let cell = new Cell(x, y, true);
+			row.push(cell);
+		}
+		table.push(row);
 	}
-	rows.push(column);
-	game.innerHTML += '<br />';
 }
+
+function tick() {
+	console.log("tick");
+	render();
+}
+
+function render() {
+	
+	
+	for (let y = 0; y < tableSize; y++) {
+		for (let x = 0; x < tableSize; x++) {
+			table[x][y].draw();
+		}
+	}
+}
+
+function start() {
+	console.log("Starting");
+	init();
+	setInterval(tick, tickDelay);
+}
+
+class Cell {
+	
+	constructor(x, y, isAlive) {
+		//The x and y values are table indices, not pixel values
+		this.x = x;
+		this.y = y;
+		this.isAlive = isAlive;
+	}
+	
+	draw() {
+		
+		if (this.isAlive) {
+			ctx.fillStyle = colors.alive;
+		} else {
+			ctx.fillStyle = colors.dead;
+		}
+		
+		let margin = 1;
+
+		ctx.fillRect(this.x * cellSize + (this.x * margin), this.y * cellSize + (this.y * margin), cellSize, cellSize);
+	}
+}
+
+init();
