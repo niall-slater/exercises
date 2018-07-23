a4f753562fb1f9f5e417dbfc8d7a26b04ca8ff48
Niall Slater <niallslater@gmail.com>
2018-07-23 16:49:54 +0100

    Initial commit - treasure

---
+++ treasure/js/main.js
@@ -0,0 +1,15 @@
+let display = document.getElementById('display');
+
+let input = [4,4,4];
+
+function solve(treasure) {
+    
+    let result = '?';
+    
+    
+    
+    display.textContent = result;
+    
+}
+
+solve(input);
