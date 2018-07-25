8d2c785487e092934d2938f1b141a568cf19e8c4
niall-slater <niallslater@gmail.com>
2018-07-25 18:48:59 +0100

    Added initial check for divisibility

--- treasure/js/main.js
+++ treasure/js/main.js
@@ -1,15 +1,39 @@
 let display = document.getElementById('display');
 
-let input = [4,4,4];
+let treasureInput = [4,4,4];
+let numCrewInput = 3;
 
-function solve(treasure) {
+function solve(treasure, numCrew) {
     
     let result = '?';
     
-    
+	let totalValue = 0;
+	
+	//Add up the total value of the treasure
+	
+    for (let i = 0; i < treasure.length; i++) {
+		
+		currentGemValue = treasure[i];
+		
+		totalValue += currentGemValue;
+		
+	}
+	
+	console.log('Total value of treasure is £' + totalValue);
+	
+	//Check to see if the total value just won't divide between them
+	
+	if (totalValue % numCrew != 0) {
+		console.log("Can't be divided equally, abort. FAIL 1");
+		display.textContent = 'NO';
+		return;
+	}
+	
+	
+	
     
     display.textContent = result;
     
 }
 
-solve(input);
+solve(treasureInput, numCrewInput);
